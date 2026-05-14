3896b87e3d4dc92581a35d339c44df17e5a42609
ulasanekonomi-collab <ulasanekonomi@gmail.com>
2026-05-14 16:22:15 +0700

    Create dictionaries.py

---
+++ modules/dictionaries.py
@@ -0,0 +1,12 @@
+RELATION_KEYWORDS = {
+
+    "bekerja sama": 3,
+    "mendukung": 2,
+    "berkolaborasi": 3,
+
+    "menolak": -3,
+    "konflik": -4,
+    "melawan": -4,
+    "bertentangan": -3,
+
+}
