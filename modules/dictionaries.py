3e32f38fb212e17047d84ee34940845c3b9adcdf
ulasanekonomi-collab <ulasanekonomi@gmail.com>
2026-05-14 19:59:29 +0700

    Update dictionaries.py

--- modules/dictionaries.py
+++ modules/dictionaries.py
@@ -1,12 +1,28 @@
 RELATION_KEYWORDS = {
 
-    "bekerja sama": 3,
-    "mendukung": 2,
-    "berkolaborasi": 3,
+    "conflict": {
+        "menolak": -3,
+        "melawan": -4,
+        "konflik": -4,
+        "bertentangan": -3,
+    },
 
-    "menolak": -3,
-    "konflik": -4,
-    "melawan": -4,
-    "bertentangan": -3,
+    "influence": {
+        "mempengaruhi": 4,
+        "menekan": 4,
+        "mendorong": 3,
+    },
+
+    "collaboration": {
+        "bekerja sama": 3,
+        "berkolaborasi": 3,
+        "mendukung": 2,
+    },
+
+    "power": {
+        "mengendalikan": 5,
+        "menguasai": 5,
+        "dominan": 4,
+    }
 
 }
