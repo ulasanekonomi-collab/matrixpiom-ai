2f5a703b356ed6aa8f06553ec5e7f3de001c9470
ulasanekonomi-collab <ulasanekonomi@gmail.com>
2026-05-14 16:49:42 +0700

    Update matrix_builder.py

--- modules/matrix_builder.py
+++ modules/matrix_builder.py
@@ -4,12 +4,12 @@ import pandas as pd
 def create_empty_matrix(actors):
 
     matrix = pd.DataFrame(
-        "",
+        0,
         index=actors,
         columns=actors
     )
 
     for actor in actors:
-        matrix.loc[actor, actor] = "-"
+        matrix.loc[actor, actor] = 0
 
     return matrix
