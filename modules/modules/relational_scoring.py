753e9878950f0e4dfae382d2316103c95b798fd7
ulasanekonomi-collab <ulasanekonomi@gmail.com>
2026-05-14 16:29:02 +0700

    Update relational_scoring.py

--- modules/modules/relational_scoring.py
+++ modules/modules/relational_scoring.py
@@ -16,3 +16,22 @@ def detect_relation_score(text):
             detected_keywords.append(keyword)
 
     return score, detected_keywords
+
+from modules.dictionaries import RELATION_KEYWORDS
+
+
+def detect_relation_score(text):
+
+    score = 0
+
+    detected_keywords = []
+
+    for keyword, value in RELATION_KEYWORDS.items():
+
+        if keyword in text.lower():
+
+            score += value
+
+            detected_keywords.append(keyword)
+
+    return score, detected_keywords
