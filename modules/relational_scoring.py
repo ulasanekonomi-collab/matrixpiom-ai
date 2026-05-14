f1091ff1a5c39bdc1be5a2a304ea9d92b9799072
ulasanekonomi-collab <ulasanekonomi@gmail.com>
2026-05-14 21:32:55 +0700

    Update relational_scoring.py

--- modules/relational_scoring.py
+++ modules/relational_scoring.py
@@ -1,21 +1,40 @@
 from modules.dictionaries import RELATION_KEYWORDS
 
-def detect_relation_score(text):
+def detect_pairwise_relations(text, actors):
 
-    detected_relations = []
+    relations = []
 
     text = text.lower()
 
     for relation_type, keywords in RELATION_KEYWORDS.items():
 
-        for keyword, value in keywords.items():
+        for keyword, score in keywords.items():
 
             if keyword in text:
 
-                detected_relations.append({
-                    "type": relation_type,
-                    "keyword": keyword,
-                    "score": value
-                })
+                for source in actors:
 
-    return detected_relations
+                    for target in actors:
+
+                        if source != target:
+
+                            source_lower = source.lower()
+                            target_lower = target.lower()
+
+                            if source_lower in text and target_lower in text:
+
+                                source_index = text.find(source_lower)
+                                target_index = text.find(target_lower)
+                                keyword_index = text.find(keyword)
+
+                                if source_index < keyword_index < target_index:
+
+                                    relations.append({
+                                        "source": source,
+                                        "target": target,
+                                        "keyword": keyword,
+                                        "score": score,
+                                        "relation_type": relation_type
+                                    })
+
+    return relations
